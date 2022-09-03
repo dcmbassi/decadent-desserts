3d4e71fb5a5862030414aab73a5a09cb2d88f651
Dave Mbassi <dcmbassi@gmail.com>
2022-09-03 20:44:41 +0100

    fix: Insert navbar in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import About from './pages/About'
+import MainNavigation from './components/MainNavigation'
 
 function App() {
 
-  return (
-    <div className="App">
-      <h1>Decadent Desserts</h1>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/products' element={<Products />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </div>
-  )
+    return (
+        <div className="App">
+            <MainNavigation />
+            
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/products' element={<Products />} />
+                <Route path='/about' element={<About />} />
+            </Routes>
+        </div>
+    )
 }
 
 export default App
