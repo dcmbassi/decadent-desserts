45c3ac8ed8a79569df70e3d0dc4a904ed1e6e645
Dave Mbassi <dcmbassi@gmail.com>
2022-09-03 21:18:48 +0100

    fix: Display products

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,10 +1,18 @@
+import { products } from "../data/products"
 
 const Products = () => {
+
     return (
         <>
             <h2>Products</h2>
-            <div>Products page</div>
-            {/* Products grid */}
+            <div className="productGrid">
+                {products.map(product => (
+                    <div className="productCard" key={product.id}>
+                        <h4>{product.name}</h4>
+                        <p>{product.price}</p>
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
