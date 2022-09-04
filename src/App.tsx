d61955af94ded97da4f4ee0180254fdbacfdff33
Dave Mbassi <dcmbassi@gmail.com>
2022-09-04 12:25:15 +0100

    fix: Wrap application inside cart context provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Home from './pages/Home'
 import Products from './pages/Products'
 import About from './pages/About'
 import MainNavigation from './components/MainNavigation'
+import { CartProvider } from './context/CartContext'
 
 function App() {
 
     return (
-        <div className="App">
-            <MainNavigation />
-            
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='/products' element={<Products />} />
-                <Route path='/about' element={<About />} />
-            </Routes>
-        </div>
+        <CartProvider>
+            <div className="App">
+                <MainNavigation />
+
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/products' element={<Products />} />
+                    <Route path='/about' element={<About />} />
+                </Routes>
+            </div>
+        </CartProvider>
     )
 }
 
