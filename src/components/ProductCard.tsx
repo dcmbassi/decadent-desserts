95f76fcd18d2eb74957dd75f8cfb3c0fe098ae13
Dave Mbassi <dcmbassi@gmail.com>
2022-09-04 15:08:21 +0100

    fix: Complete markup and styling of ProductCard component

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -4,11 +4,24 @@ type CardProps = {
     product: Product
 }
 
-const ProductCard = ({product}: CardProps) => {
+const ProductCard = ({ product }: CardProps) => {
     return (
         <div className="productCard">
-            <h4>{product.name}</h4>
-            <p>{product.price}</p>
+            <div className="cardInfo">
+                <h4>{product.name}</h4>
+                <p>{product.price}</p>
+            </div>
+            <div className="cardActions">
+                <div className="cartManip">
+                    <button className="cartQuantity">-</button>
+                    <div>2</div>
+                    <button className="cartQuantity">+</button>
+                </div>
+                <div className="cardMainAction">
+                    <button className="cardMainAction">Ajouter au panier</button>
+                    <button className="cardMainAction">Retirer du panier</button>
+                </div>
+            </div>
         </div>
     )
 }
