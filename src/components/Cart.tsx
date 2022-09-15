a8804fc87053069ec03b06c499ee6c8e0df2a969
Dave Mbassi <dcmbassi@gmail.com>
2022-09-15 23:32:25 +0100

    fix: Import CartItem in Cart component

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,18 +1,22 @@
 import { useCart } from '../context/CartContext'
+import CartItem from './Cartitem'
 
 type CartProps = {
     isOpen: boolean
 }
 
 const Cart = ({ isOpen }: CartProps) => {
-    const { closeCart } = useCart()
+    const { closeCart, cartItems } = useCart()
     const cartClassName = isOpen ? 'cart' : 'cart hidden'
 
     return (
         <div className={cartClassName}>
-            <div className="cartContent">
+            <div className="cartHeader">
                 <h5>Pannier</h5>
                 <button onClick={closeCart}>Close</button>
+            </div>
+            <div className="cartContent">
+                <CartItem />
             </div>
         </div>
     )
