9ff2b42d4d7148a79cfcae946aedda36566ebef5
Dave Mbassi <dcmbassi@gmail.com>
2022-09-24 10:20:27 +0100

    fix: Set up CSS module for ProductCard component

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,49 +1,56 @@
 import { useCart } from "../../context/CartContext"
 import { Product } from "../../data/products"
 import { formatCurrency } from "../../utils/formatCurrency"
+import styles from './ProductCard.module.css'
 
 type CardProps = {
     product: Product
 }
 
 const ProductCard = ({ product }: CardProps) => {
-    const { getItemQuantity, increaseItemQuantity, decreaseItemQuantity, removeItem } = useCart()
+    const { 
+        getItemQuantity, 
+        increaseItemQuantity, 
+        decreaseItemQuantity, 
+        removeItem 
+    } = useCart()
     const quantity = getItemQuantity(product.id)
+    
     return (
-        <div className="productCard">
-            <div className="cardInfo">
-                <div className="cardImage">
+        <div className={styles.productCard}>
+            <div className={styles.cardInfo}>
+                <div className={styles.cardImage}>
                     <img
                         src={product.imageUrl}
                         alt={product.name}
-                        className='productCardImage'
+                        className={styles.productCardImage}
                     />
                 </div>
                 <h4>{product.name}</h4>
                 <p>{formatCurrency(product.price)}</p>
             </div>
-            <div className="cardActions">
+            <div className={styles.cardActions}>
                 {!!quantity && (
-                    <div className="cartManip">
+                    <div className={styles.cartManip}>
                         <button
-                            className="cartQuantity"
+                            className={styles.cartQuantity}
                             onClick={() => decreaseItemQuantity(product.id)}
                         >
                             -
                         </button>
                         <div>{quantity}</div>
                         <button
-                            className="cartQuantity"
+                            className={styles.cartQuantity}
                             onClick={() => increaseItemQuantity(product.id)}
                         >
                             +
                         </button>
                     </div>
                 )}
-                <div className="cardMainAction">
+                <div className={styles.cardMainAction}>
                     {!!!quantity && (
                         <button
-                            className="cardMainAction"
+                            className={styles.cardMainAction}
                             onClick={() => increaseItemQuantity(product.id)}
                         >
                             Ajouter au panier
@@ -51,7 +58,7 @@ const ProductCard = ({ product }: CardProps) => {
                     )}
                     {!!quantity && (
                         <button
-                            className="cardMainAction"
+                            className={styles.cardMainAction}
                             onClick={() => removeItem(product.id)}
                         >
                             Retirer du panier
