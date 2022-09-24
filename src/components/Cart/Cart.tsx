e6f32650cc5dc5609e53be3a8db1eeaddf325d4d
Dave Mbassi <dcmbassi@gmail.com>
2022-09-24 09:21:05 +0100

    fix: Move Cart component to dedicated folder

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,8 +1,9 @@
-import { useCart } from '../context/CartContext'
-import { formatCurrency } from '../utils/formatCurrency'
-import { products } from '../data/products'
-import CartItem from './CartItem'
+import { useCart } from '../../context/CartContext'
+import { formatCurrency } from '../../utils/formatCurrency'
+import { products } from '../../data/products'
+import CartItem from '../CartItem'
 import React from 'react'
+import styles from './Cart.module.css'
 
 type CartProps = {
     isOpen: boolean
@@ -10,8 +11,8 @@ type CartProps = {
 
 const Cart = ({ isOpen }: CartProps) => {
     const { closeCart, cartItems } = useCart()
-    const cartClassName = isOpen ? 'cart' : 'cart hidden'
-    const overayClassName = isOpen ? 'cartOverlay' : '' 
+    const cartClassName = isOpen ? styles.cart : `${styles.cart} ${styles.hidden}`
+    const overayClassName = isOpen ? styles.cartOverlay : '' 
 
     const handleOutsideInteraction = (e: React.MouseEvent<HTMLElement>) => {
         closeCart()
@@ -23,15 +24,15 @@ const Cart = ({ isOpen }: CartProps) => {
     return (
         <div className={overayClassName} onClick={handleOutsideInteraction}>
             <div className={cartClassName} onClick={stopEventPropagation}>
-                <div className="cartHeader">
+                <div className={styles.cartHeader}>
                     <h3>Panier</h3>
                     <button onClick={closeCart}>&times;</button>
                 </div>
-                <div className="cartContent">
+                <div className={styles.cartContent}>
                     {cartItems.map(item => (
                         <CartItem key={item.id} {...item} />
                     ))}
-                    <div className="cartTotal">
+                    <div className={styles.cartTotal}>
                         Total: {formatCurrency(cartItems.reduce((total, cartItem) => {
                             const item = products.find(product => product.id === cartItem.id)
                             return total + (item?.price || 0) * cartItem.quantity
