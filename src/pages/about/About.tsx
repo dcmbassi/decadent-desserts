e2cd39960fd6b567b28e1d92b68098785a451664
Dave Mbassi <dcmbassi@gmail.com>
2022-09-24 17:23:31 +0100

    fix: Correct typo

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,7 +3,7 @@ import styles from './About.module.css'
 const About = () => {
     return (
         <>
-            <h2 className={styles.heading}>Aboout Us</h2>
+            <h2 className={styles.heading}>About Us</h2>
             <div>About Page</div>
         </>
     )
