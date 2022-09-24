0c0dc55c4d035c8c208e0ae0d0415f1fad3bfad4
Dave Mbassi <dcmbassi@gmail.com>
2022-09-24 10:02:54 +0100

    fix: Set up CSS module for MainNavigation component

--- src/components/MainNav/MainNavigation.tsx
+++ src/components/MainNav/MainNavigation.tsx
@@ -2,27 +2,32 @@ import ddLogo from '../../img/dd_logo.jpeg'
 import cart from '../../img/cart.png'
 import { NavLink } from 'react-router-dom'
 import { useCart } from '../../context/CartContext'
+import styles from './MainNavigation.module.css'
 
 const MainNavigation = () => {
     const { openCart, cartQuantity } = useCart()
     return (
-        <nav className="mainNav">
-            <div className="logo">
+        <nav className={styles.mainNav}>
+            <div className={styles.logo}>
                 <img src={ddLogo} alt="Decadent Desserts" />
             </div>
-            <ul className='navMenu'>
+            <ul className={styles.navMenu}>
                 <NavLink to='/'>
-                    <li className='navItem'>Accueil</li>
+                    <li>Accueil</li>
                 </NavLink>
                 <NavLink to='/products'>
-                    <li className='navItem'>Produits</li>
+                    <li>Produits</li>
                 </NavLink>
                 <NavLink to='/about'>
-                    <li className='navItem'>A propos</li>
+                    <li>A propos</li>
                 </NavLink>
             </ul>
             {!!cartQuantity && (
-                <button onClick={openCart} className='cartButton' data-value={cartQuantity}>
+                <button
+                    onClick={openCart}
+                    className={styles.cartButton}
+                    data-value={cartQuantity}
+                >
                     <img src={cart} alt='Shopping cart' />
                 </button>
             )}
