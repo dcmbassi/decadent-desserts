29e732aa15336e67a3969947e352ce26d473fb81
Dave Mbassi <dcmbassi@gmail.com>
2022-09-27 16:45:07 +0100

    feat: Animate header

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,9 +3,11 @@ import styles from './Home.module.css'
 const Home = () => {
     return (
         <>
-            <header className={styles.hero}>
+            <header className={`${styles.hero} ${styles.fadeIn}`}>
                 <div className={styles.overlay}>
-                    <h1 className={styles.heading}>Decadent Desserts</h1>
+                    <h1 className={`${styles.heading} ${styles.textFade}`}>
+                        Decadent Desserts
+                    </h1>
                     <p className={styles.tagline}>Unapologetically delicious</p>
                 </div>
             </header>
